aaaee7424c024f28d6962ddc3f703cb8aa9abf0d
Nicole Cordes <typo3@cordes.co>
2017-12-17 19:42:36 +0100

    [FEATURE] Add (very) basic log command

---
+++ src/PhpunitMerger/Command/LogCommand.php
@@ -0,0 +1,139 @@
+<?php
+namespace Nimut\PhpunitMerger\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+
+class LogCommand extends Command
+{
+    /**
+     * @var \DOMDocument
+     */
+    private $document;
+
+    /**
+     * @var \DOMElement[]
+     */
+    private $domElements = [];
+
+    protected function configure()
+    {
+        $this->setName('log')
+            ->setDescription('Merges multiple PHPUnit JUnit xml files into one')
+            ->addArgument(
+                'directory',
+                InputArgument::REQUIRED,
+                'The directory containing PHPUnit JUnit xml files'
+            )
+            ->addArgument(
+                'file',
+                InputArgument::REQUIRED,
+                'The file where to write the merged result'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $finder = new Finder();
+        $finder->files()
+            ->in(realpath($input->getArgument('directory')));
+
+        $this->document = new \DOMDocument('1.0', 'UTF-8');
+        $this->document->formatOutput = true;
+
+        $root = $this->document->createElement('testsuites');
+        $this->document->appendChild($root);
+
+        foreach ($finder as $file) {
+            $xml = new \SimpleXMLElement(file_get_contents($file->getRealPath()));
+            $xmlArray = json_decode(json_encode($xml), true);
+            if (!empty($xmlArray)) {
+                $this->addTestSuites($root, $xmlArray);
+            }
+        }
+
+        foreach ($this->domElements as $domElement) {
+            if ($domElement->hasAttribute('parent')) {
+                $domElement->removeAttribute('parent');
+            }
+        }
+
+        $this->document->save($input->getArgument('file'));
+    }
+
+    private function addTestSuites(\DOMElement $parent, array $testSuites)
+    {
+        foreach ($testSuites as $testSuite) {
+            $attributes = $testSuite['@attributes'] ?? [];
+            if (empty($testSuite['@attributes']['name'])) {
+                continue;
+            }
+            $name = $testSuite['@attributes']['name'];
+
+            if (isset($this->domElements[$name])) {
+                $element = $this->domElements[$name];
+            } else {
+                $element = $this->document->createElement('testsuite');
+                $element->setAttribute('parent', $parent->getAttribute('name'));
+                foreach ($attributes as $key => $value) {
+                    $value = $key === 'name' ? $value : 0;
+                    $element->setAttribute($key, $value);
+                }
+                $parent->appendChild($element);
+                $this->domElements[$name] = $element;
+            }
+
+            if (!empty($testSuite['testsuite'])) {
+                $children = isset($testSuite['testsuite']['@attributes']) ? [$testSuite['testsuite']] : $testSuite['testsuite'];
+                $this->addTestSuites($element, $children);
+            }
+
+            if (!empty($testSuite['testcase'])) {
+                $children = isset($testSuite['testcase']['@attributes']) ? [$testSuite['testcase']] : $testSuite['testcase'];
+                $this->addTestCases($element, $children);
+            }
+        }
+    }
+
+    private function addTestCases(\DOMElement $parent, array $testCases)
+    {
+        foreach ($testCases as $testCase) {
+            $attributes = $testCase['@attributes'] ?? [];
+            if (empty($testCase['@attributes']['name'])) {
+                continue;
+            }
+            $name = $testCase['@attributes']['name'];
+
+            if (isset($this->domElements[$name])) {
+                continue;
+            }
+
+            $element = $this->document->createElement('testcase');
+            foreach ($attributes as $key => $value) {
+                $element->setAttribute($key, $value);
+                if (!is_numeric($value)) {
+                    continue;
+                }
+                $this->addAttributeValueToTestSuite($parent, $key, $value);
+            }
+            $parent->appendChild($element);
+            $this->domElements[$name] = $element;
+        }
+    }
+
+    private function addAttributeValueToTestSuite(\DOMElement $element, $key, $value)
+    {
+        $currentValue = $element->hasAttribute($key) ? $element->getAttribute($key) : 0;
+        $element->setAttribute($key, $currentValue + $value);
+
+        if ($element->hasAttribute('parent')) {
+            $parent = $element->getAttribute('parent');
+            if (isset($this->domElements[$parent])) {
+                $this->addAttributeValueToTestSuite($this->domElements[$parent], $key, $value);
+            }
+        }
+    }
+}
