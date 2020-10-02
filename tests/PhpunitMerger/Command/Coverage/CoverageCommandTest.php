f42f8ccf92539545c01e3fb71eaa2ee523f975e0
Nicole Cordes <typo3@cordes.co>
2020-10-02 18:40:48 +0200

    Ensure unit test checks own boundaries usage

--- tests/PhpunitMerger/Command/Coverage/CoverageCommandTest.php
+++ tests/PhpunitMerger/Command/Coverage/CoverageCommandTest.php
@@ -96,6 +96,16 @@ class CoverageCommandTest extends AbstractCommandTest
         $command->run($input, $output->reveal());
 
         $this->assertFileExists($this->logDirectory . $this->outputFile);
+
+        $content = file_get_contents($this->logDirectory . $this->outputFile);
+        if (method_exists($this, 'assertStringContainsString')) {
+            $this->assertStringContainsString('<strong>Low</strong>: 0% to 20%', $content);
+            $this->assertStringContainsString('<strong>High</strong>: 70% to 100%', $content);
+        } else {
+            // Fallback for phpunit < 7.0
+            $this->assertContains('<strong>Low</strong>: 0% to 20%', $content);
+            $this->assertContains('<strong>High</strong>: 70% to 100%', $content);
+        }
     }
 
     public function testCoverageWritesOutputFileAndHtmlReport()
